44ebbf3b631af4c375ff451bb0bde9cc18c0e51d
Grigory Polkhovsky <griiha11@yandex.ru>
2024-04-21 20:18:47 +0300

    Сдача проекта

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content; 
 
 // @todo: DOM узлы
+const list = document.querySelector('.places__list');
+const card = document.querySelector('.places__item card');
+const deleteButton = cardTemplate.querySelector('.card__delete-button');
 
 // @todo: Функция создания карточки
+function createCard(name, link, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard)
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__image').alt = name;
+    cardElement.querySelector('.card__title').textContent = name;
+
+    return cardElement
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+    const removedCard = event.target.closest('.card');
+    removedCard.remove()
+}
+
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach (function(item) {
+    list.append(createCard(item.name, item.link, deleteCard))
+})
